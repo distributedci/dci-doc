1d7b3afc5b6039de8b3227ae820e307dfec2fca8
Frederic Lepied <flepied@redhat.com>
2022-06-24 09:18:14 +0200

    build.sh: get all the markdown files
    Change-Id: I8caa5c8e3049e02ab93cbe0c5f5d706fec5bee42

--- build.sh
+++ build.sh
@@ -1,27 +1,24 @@
 #!/usr/bin/env bash
 set -eux
 temp_folder="/tmp/dci-doc"
-rm -rf ${temp_folder}
-mkdir ${temp_folder}
-cp -r . ${temp_folder}
+rm -rf "${temp_folder}"
+mkdir "${temp_folder}"
+cp -r . "${temp_folder}"
 projects="dci-control-server dci-ui python-dciclient python-dciauth dci-downloader dci-openstack-agent dci-rhel-agent dci-openshift-agent ansible-playbook-dci-beaker dci-openshift-app-agent dci-pipeline"
-for project in ${projects}
-do
-    echo ${project}
-    rm -rf ${temp_folder}/src/${project}
-    mkdir ${temp_folder}/src/${project}
-    cp ../${project}/README* ${temp_folder}/src/${project}/
-    cp -r ../${project}/docs/ ${temp_folder}/src/${project}/docs/ 2>/dev/null || true
+for project in ${projects}; do
+    echo "${project}"
+    mkdir "${temp_folder}/src/${project}"
+    (tar c -C "../${project}" $(cd "../${project}"; find . -name "*.md"|grep -v "^\./\.")) | tar xv -C "${temp_folder}/src/${project}/"
 done
 # dci openshift app agent roles
 mkdir -p ${temp_folder}/src/dci-openshift-app-agent/roles/
 pushd ../dci-openshift-app-agent
 for f in roles/*; do
-  cp -r "${f}" ${temp_folder}/src/dci-openshift-app-agent/${f}/
+    cp -r "${f}" ${temp_folder}/src/dci-openshift-app-agent/${f}/
 done
 popd
 
-pushd ${temp_folder}
+pushd "${temp_folder}"
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
@@ -29,5 +26,5 @@ mkdocs build
 popd
 rm -rf ./docs
 mkdir ./docs
-cp -r ${temp_folder}/docs/ ./
-rm -rf ${temp_folder}
+cp -r "${temp_folder}/docs/" ./
+rm -rf "${temp_folder}"
